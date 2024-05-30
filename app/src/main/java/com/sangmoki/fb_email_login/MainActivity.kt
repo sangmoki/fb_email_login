d5f5a271bf131b965cff5c550d4cc12c8f9c39f7
이상목 <yoochang4176@naver.com>
2024-05-30 15:38:58 +0900

    Feat: "로그인, 로그아웃 기능 구현 완료"

--- app/src/main/java/com/sangmoki/fb_email_login/MainActivity.kt
+++ app/src/main/java/com/sangmoki/fb_email_login/MainActivity.kt
@@ -27,22 +27,23 @@ class MainActivity : AppCompatActivity() {
         }
 
         // Firebase 사용자 현재 로그인 확인
-        var currentUser = auth.currentUser
-
-        // Firebase 인증 객체
-        auth = Firebase.auth
+        auth = FirebaseAuth.getInstance()
 
         // 회원가입 버튼
         val joinBtn = findViewById<Button>(R.id.joinBtn)
         // 로그인 버튼
         val loginBtn = findViewById<Button>(R.id.loginBtn)
+        // 로그아웃 버튼        
+        val logoutBtn = findViewById<Button>(R.id.logoutBtn)
+
+        // 이메일과 패스워드 정보
+        val email = findViewById<EditText>(R.id.emailArea)
+        val password = findViewById<EditText>(R.id.passwordArea)
         
         // 회원가입 버튼 클릭 이벤트        
         joinBtn.setOnClickListener {
            
-            // 이메일과 패스워드를 가져온다
-            val email = findViewById<EditText>(R.id.emailArea)
-            val password = findViewById<EditText>(R.id.passwordArea)
+
 
             // 인증객체에 email과 password를 넘겨 신규 계정을 생성한다.
             auth.createUserWithEmailAndPassword(email.text.toString(), password.text.toString())
@@ -50,17 +51,52 @@ class MainActivity : AppCompatActivity() {
 
                     // 성공했을 경우 회원가입
                     if (task.isSuccessful) {
-                        val user = auth.currentUser
-                        Log.d("User", user.toString())
+                        Toast.makeText(
+                            baseContext,
+                            "회원가입이 완료되었습니다.",
+                            Toast.LENGTH_SHORT,
+                        ).show()
                     // 실패했을 경우 실패 메시지 출력
                     } else {
                         Toast.makeText(
                             baseContext,
-                            "Authentication failed.",
+                            "회원가입에 실패하였습니다.",
                             Toast.LENGTH_SHORT,
                         ).show()
                     }
                 }
         }
+        
+        loginBtn.setOnClickListener {
+            auth.signInWithEmailAndPassword(email.text.toString(), password.text.toString())
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        Toast.makeText(
+                            baseContext,
+                            "로그인 성공",
+                            Toast.LENGTH_SHORT,
+                        ).show()
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Toast.makeText(
+                            baseContext,
+                            "로그인 실패",
+                            Toast.LENGTH_SHORT,
+                        ).show()
+                    }
+                }
+        }
+
+        // 로그아웃 버튼 클릭 시 발생 이벤트
+        logoutBtn.setOnClickListener {
+            auth.signOut()
+            Toast.makeText(
+                baseContext,
+                "로그아웃 완료",
+                Toast.LENGTH_SHORT,
+            ).show()
+        }
+
+
     }
 }
